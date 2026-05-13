c014c5172710956571969e90505b4c65d7c42077
huseynlin <nuralihuseynli53@gmail.com>
2026-05-13 13:06:37 +0400

    delete

--- BankSystem/BankSystem/Models/ExchangeRate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BankSystem.Models
-{
-    public class ExchangeRate
-    {
-        public string CurrencyCode { get; set; } // USD, EUR və s.
-        public decimal RateToAZN { get; set; }  // 1 vahidin neçə AZN olduğu (məs: 1.70)
-    }
-}
